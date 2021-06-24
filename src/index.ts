18be8bfc5df03f5a2e9fe739aceaac95dfc06c76
Shermayne Lee <shermayne.lee@mechanicalrock.io>
2021-06-24 08:38:10 +0800

    Add logging for debugging

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import * as core from "@actions/core";
 async () => {
   const versionFromBranch = extractPackageVersionFromBranch("./package.xml");
   const versionFromMaster = await extractPackageVersionFromMaster();
-
+  console.log(
+    "Versions from branch and mater",
+    versionFromBranch,
+    versionFromMaster
+  );
   return versionFromBranch === versionFromMaster
     ? core.setFailed(
         "Action failed due to the package version between main and the branch are same. Please bump the package version"
