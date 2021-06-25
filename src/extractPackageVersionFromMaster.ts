1b24332a20157195d9f7c3ac3efbb738587ba337
Shermayne Lee <shermayne.lee@mechanicalrock.io>
2021-06-25 11:22:17 +0800

    Adding logging for git command

--- src/extractPackageVersionFromMaster.ts
+++ src/extractPackageVersionFromMaster.ts
@@ -1,14 +1,25 @@
 import simpleGit, { SimpleGit } from "simple-git";
 import * as parser from "xml2json";
-import * as core from "@actions/core";
+
 import { extractVersionFromJSON } from "./extractPackageVersionFromBranch";
 
 export const extractPackageVersionFromMaster = async () => {
   const git: SimpleGit = simpleGit();
+  console.log("git", await git.remote(["-v"]));
+  console.log("git", await git.status());
+  const branch = await git.branch();
+  console.log("git", branch.current);
+  let packageXMLFile;
+  try {
+    packageXMLFile = await git.show("main:package.xml");
+  } catch (e) {
+    throw e;
+  }
 
-  const packageXMLFile = await git.show("main:package.xml");
-  core.info(`XML master, ${packageXMLFile}`);
+  console.log(`XML master, ${packageXMLFile}`);
   const packageJSON = parser.toJson(packageXMLFile);
 
   return extractVersionFromJSON(JSON.parse(packageJSON));
 };
+
+extractPackageVersionFromMaster();
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import * as core from "@actions/core";
 
 const handler = async () => {
   const versionFromBranch = extractPackageVersionFromBranch("./package.xml");
+  console.log(`Versions from branchhhh, ${versionFromBranch}`);
   const versionFromMaster = await extractPackageVersionFromMaster();
-  core.info(
+  console.log(
     `Versions from branch and mater, ${versionFromBranch}, ${versionFromMaster}`
   );
   console.log(
